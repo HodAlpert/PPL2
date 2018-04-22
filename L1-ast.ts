91a6809d544cf7a3c0d05051dce3d3d933bf8618
alperth@post.bgu.ac.il <246315>
2018-04-22 18:11:15 +0300

    commit

--- L1-ast.ts
+++ L1-ast.ts
@@ -3,6 +3,8 @@
 import * as assert from "assert";
 import { filter, map } from "ramda";
 
+deandean
+
 export type Exp = DefineExp | CExp;
 export type CExp = NumExp | BoolExp | PrimOp | VarRef | VarDecl | AppExp | Error;
 
